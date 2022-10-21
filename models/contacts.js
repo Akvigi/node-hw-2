f3b3b2f7f361fb0872e72ec914500d9cde346c0b
Nikita P <nikita.panasevich22@gmail.com>
2022-10-21 08:33:16 +0300

    added getquery

--- models/contacts.js
+++ models/contacts.js
@@ -1,8 +1,19 @@
-// const fs = require('fs/promises')
+const fs = require('fs/promises')
+const path = require('path');
+const contacts = path.join(__dirname, 'contacts.json');
 
-const listContacts = async () => {}
+const listContacts = async () => {
+  const data = fs.readFile(contacts, 'utf-8').then(data => {return JSON.parse(data)});
+  return data
+}
 
-const getContactById = async (contactId) => {}
+const getContactById = async contactId => {
+  const data = fs.readFile(contacts, 'utf-8').then(data => {
+    const contact = JSON.parse(data).find(item => Number(item.id) === Number(contactId));
+    return contact
+  });
+  return data
+};
 
 const removeContact = async (contactId) => {}
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,13 +1,28 @@
 const express = require('express')
 
+const contacts = require('../../models/contacts');
+
 const router = express.Router()
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/', async (req, res) => {
+  try {
+    const data = await contacts.listContacts()
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: "Servor error"})
+  }
 })
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+router.get('/:contactId', async (req, res) => {
+  try {
+    const data = await contacts.getContactById(req.params.contactId)
+    if (!data) {
+      return res.status(404).json({message: "404"})
+    }
+    res.json(data)
+  } catch (error) {
+    res.status(500).json({ message: "Servor error"})
+  }
 })
 
 router.post('/', async (req, res, next) => {
